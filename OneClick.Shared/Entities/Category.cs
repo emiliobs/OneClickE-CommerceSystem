3d4c30546d6f31dee3939d24ecdca31f3155b014
Emilio Barrera <barrera_emilio@yahoo.es>
2025-12-26 21:33:06 +0000

    Add project files.

---
+++ OneClick.Shared/Entities/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace OneClick.Shared.Entities;
+
+public class Category
+{
+    public int Id { get; set; }
+
+    [MaxLength(100, ErrorMessage = "Field {0} cannot have more than {1} characters.")]
+    [Required(ErrorMessage = "Field {0} is required.")]
+    public string Name { get; set; } = null!;
+
+    // Navigation property
+    public List<Product> Products { get; set; } = new List<Product> { };
+}
---
+++ OneClick.Shared/Entities/Product.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace OneClick.Shared.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    [MaxLength(100, ErrorMessage = "Field {0} cannot have more than {1} characters.")]
+    [Required(ErrorMessage = "Field {0} is required.")]
+    public string Name { get; set; } = string.Empty;
+
+    [StringLength(1000)]
+    public string Description { get; set; } = string.Empty;
+
+    [StringLength(500)]
+    public string ImageURL { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "Field {0} is required.")]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal Price { get; set; }
+
+    [Required(ErrorMessage = "Field {0} is required.")]
+    [Range(0, int.MaxValue)]
+    public int Qty { get; set; }
+
+    [Required(ErrorMessage = "Field {0} is required.")]
+    public int CategoryId { get; set; }
+
+    // Navigation property
+    [ForeignKey("CategoryId")]
+    public Category? Category { get; set; }
+}
